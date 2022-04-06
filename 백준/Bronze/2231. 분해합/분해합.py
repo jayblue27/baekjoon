2c2c92cdec6a011f5bf9b1dc4adc3570f9c0d22d
jayblue <jay.jeong0082@gmail.com>
2022-04-06 23:39:27 +0900

    [Bronze II] Title: 분해합, Time: 2284 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,6 +1,7 @@
+#입력
 n = input()
 
-
+# 분해합 만들기 함수
 def gen_making(n):
     lst = [int(n)]
     for i in range(len(n)):
@@ -10,9 +11,11 @@ def gen_making(n):
 
 # 숫자 순회 입력 숫자 까지
 for i in range(int(n)):
-    # 자릿수 순회 (길이만큼)
+    # 분해합 결과가 입력값이랑 같으면 출력하고 종료
+    # 0부터 올라오는거니까 가장 작은 숫자가 출력 됨 
     if gen_making(str(i)) == int(n):
         print(i)
         break
+#없으면 0 출력
 else:
     print(0)
