722d02779e3d05aa37ac25d49d8582c439830460
HoYeong Jeong <jay.jeong0082@gmail.com>
2022-04-08 18:54:32 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,20 +1,11 @@
 A,B,C = map(int, input().split())
 s = int(input())
 
+C = C+s
+B = B + (C // 60)
+C = C%60
+A = A + (B // 60)
+B = B%60
+A = A%24
 
-C += s
-
-if C >= 60:
-    B = B + (C//60)
-    C = C % 60
-
-    if B >= 60:
-        A = A + (B//60)
-        B = B % 60
-
-        if A >= 24:
-            A = A%24
-
-    
-
-print(A, B, C)
+print(A,B,C)
