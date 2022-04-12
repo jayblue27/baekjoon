ce18e661dd2196f1b917f9bead5a327559a2f238
HoYeong Jeong <jay.jeong0082@gmail.com>
2022-04-12 22:59:05 +0900

    [Silver IV] Title: 큐 2, Time: 2528 ms, Memory: 91508 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,39 @@
+# 시간초과 때문에 readline 써줘야 한다고 함 
+# colab에서는 sys.stdin.readline() 안된다고 함
+import sys
+from collections import deque
+
+N = int(input())
+queue = deque()
+for _ in range(N):
+    C = list(sys.stdin.readline().split())
+    if len(C) == 2:
+        queue.append(int(C[1]))
+    
+    elif C[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[0])
+
+    elif C[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[-1])
+
+    elif C[0] == 'size':
+        print(len(queue))
+
+    elif C[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    
+    elif C[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.popleft())
+            
