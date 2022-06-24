b7305aa84cb6d3b2ae409a2e5393f27a017ac1f3
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-06-24 23:10:39 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 80 ms, Memory: 32884 KB -BaekjoonHub

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.py
@@ -0,0 +1,17 @@
+n = int(input())
+data = list(map(int, input().split()))
+ 
+height = data[0]
+ans = []
+count = 0
+ 
+for i in range(1, n):
+  if data[i] < height:
+    count += 1
+  else:
+    height = data[i]
+    ans.append(count)
+    count = 0
+  
+ans.append(count) 
+print(max(ans))
