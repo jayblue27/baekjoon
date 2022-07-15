119f9b19bbf4a39504015a9d9035699bcb903892
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-15 19:09:48 +0900

    Time: 78 ms (12.88%), Space: 14 MB (37.14%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -16,6 +16,7 @@ class Solution:
         - re.sub(r'[^\w]', ' ', paragraph) -> paragraph 안에서 word character가 아닌경우 공백으로 변경
           - \W -> Word character
           - ^ -> not
+        - .most_common(n) 가장 빈도가 높은 n개의 요소 반환
         '''        
         
         words = [ w for w in re.sub(r'[^\w]', ' ', paragraph)    # 1. - "!?',;." 제거
