51d176b7e08d8539c82918ababd97c202d59496c
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-19 16:12:56 +0900

    Time: 643 ms (34.19%), Space: 14.9 MB (95.36%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,10 +6,27 @@ class Solution:
         
         ㅇ 접근법
           1) 부르트포스? 
-          2) 
+          2) in을 이용한 탐색  
+          3)
+          4)
         '''
-        for i in range(0,len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return i,j
+#         # 1) 부르트포스 - 7158ms
+#         # 모든 조합 검색
+#         for i in range(0,len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return i,j
+        
+        # 2) in을 이용한 탐색
+        # (target - n) 값이 nums 안에 있는지 확인
+        for i, n in enumerate(nums):
+            complement = target - n
+            
+            if complement in nums[i+1:]:
+                return [ nums.index(n), nums[i+1:].index(complement)+(i+1)]
+        
+#         # 3) 첫번째 수를 뺀 결과 키 조회
+#         nums_map = {}
+        
+#         # 4) 
                     
