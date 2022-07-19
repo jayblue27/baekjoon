1aa885c018a532d01a1d421ad2856015776986b4
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-19 19:58:59 +0900

    Time: 288 ms (10.82%), Space: 16.1 MB (41.59%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -6,44 +6,44 @@ class Solution:
         1) 투 포인터 (투 포인터를 쓰는 문제 풀이가 자주 등장한다.)
         2) 스택
         '''
-#         if not height:
-#             return 0
+        if not height:
+            return 0
         
-#         volume = 0
-#         left, right = 0, len(height)-1   # 포인터 지정
-        
-#         left_max, right_max = height[left], height[right]
-        
-#         while left < right:
-#             left_max = max(height[left], left_max)
-#             right_max = max(height[right], right_max)
-            
-#             if left_max <= right_max:
-#                 volume += left_max - height[left]
-#                 left += 1 # 오른쪽으로 이동
-#             else:
-#                 volume += right_max - height[right]
-#                 right -= 1 # 왼쪽으로 이동
-        
-#         return volume
-
-        # 2) 스택
-        stack = []
         volume = 0
+        left, right = 0, len(height)-1   # 포인터 지정
         
-        for i in range(len(height)):
-            while stack and height[i] > height[stack[-1]]:
-                top = stack.pop()
-                if not len(stack):
-                    break
+        left_max, right_max = height[left], height[right]
+        
+        while left < right:
+            left_max = max(height[left], left_max)
+            right_max = max(height[right], right_max)
             
-                dist = i - stack[-1] - 1
-                waters = min(height[i], height[stack[-1]]) - height[top]
-            
-                volume += dist * waters
-            
-            stack.append(i)
+            if left_max <= right_max:
+                volume += left_max - height[left]
+                left += 1 # 오른쪽으로 이동
+            else:
+                volume += right_max - height[right]
+                right -= 1 # 왼쪽으로 이동
+        
         return volume
+
+#         # 2) 스택 - 305ms
+#         stack = []
+#         volume = 0
+        
+#         for i in range(len(height)):
+#             while stack and height[i] > height[stack[-1]]:
+#                 top = stack.pop()
+#                 if not len(stack):
+#                     break
+            
+#                 dist = i - stack[-1] - 1
+#                 waters = min(height[i], height[stack[-1]]) - height[top]
+            
+#                 volume += dist * waters
+            
+#             stack.append(i)
+#         return volume
             
             
             
