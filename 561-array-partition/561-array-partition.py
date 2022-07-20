9239269469b54444ad28a008777b5aaced822fd9
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-20 16:20:34 +0900

    Time: 293 ms (89.18%), Space: 16.7 MB (51.12%) - LeetHub

--- 561-array-partition/561-array-partition.py
+++ 561-array-partition/561-array-partition.py
@@ -1,18 +1,25 @@
 class Solution:
     def arrayPairSum(self, nums: List[int]) -> int:
-        '''
+        '''        
         2n 길이
         nums 숫자 오름차순 정렬하고 
         홀수번째 숫자의 합 더한다.        
+        
         '''
-        # 1) 내 풀이 - 
-        # nums 오름차순 정렬
-        nums.sort()
+#         # 1) 내 풀이 - 480ms 
+#         # nums 오름차순 정렬
+#         nums.sort()
         
-        # 0번부터 2칸씩 증가하며 덧셈 실시
-        ans = 0 
-        for i in range(0,len(nums),2):
-            ans += nums[i]
+#         # 0번부터 2칸씩 증가하며 덧셈 실시
+#         # 
+#         ans = 0 
+#         for i in range(0,len(nums),2):
+#             ans += nums[i]
         
-        return ans
+#         return ans
+    
+        # 2) 파이써닉한 풀이(책3번) - 오.. 
+        return sum(sorted(nums)[::2])
+        
+        
         
