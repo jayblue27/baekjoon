0704afeb2d04aee05717f2766eefc8ebfd0316cf
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-20 17:24:46 +0900

    Time: 223 ms (99.02%), Space: 21.2 MB (43.25%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,8 +5,16 @@ class Solution:
         - 나눗셈 하지마라
         - O(n) 복잡도
         '''
-        # 1.  내 풀이 - 나눗셈 
-        
+#         import math 
+#         # 1.  내 풀이 - 시간초과
+#         # deque 써서 순환시키고 해당 숫자를 제외한 숫자끼리 곱한 값을 append
+#         deq = deque(nums)
+#         ans = []
+#         for _ in range(len(nums)):
+#             deq.append(deq.popleft())
+#             ans.append(math.prod(list(deq)[:-1]))
+            
+#         return ans
         
         # 2. 책 풀이 
         out = []
