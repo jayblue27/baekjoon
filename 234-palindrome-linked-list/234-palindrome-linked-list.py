301859d24781335c76da7e169b80a57a7d787aa7
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-25 02:29:02 +0900

    Time: 1434 ms (21.82%), Space: 47.5 MB (6.83%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -5,9 +5,24 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        # 1. 파이썬 리스트로 변환
-        q = []
+#         # 1. 파이썬 리스트로 변환 - 2569ms
+#         q = []
         
+#         if not head:
+#             return True
+        
+#         node = head
+#         # 리스트 변환
+#         while node is not None:
+#             q.append(node.val)
+#             node = node.next
+#         #팰린드롬 판별
+#         while len(q) > 1:
+#             if q.pop(0) != q.pop():
+#                 return False
+        
+        # 2. deque
+        q = collections.deque()
         if not head:
             return True
         
@@ -18,8 +33,13 @@ class Solution:
             node = node.next
         #팰린드롬 판별
         while len(q) > 1:
-            if q.pop(0) != q.pop():
+            if q.popleft() != q.pop():
                 return False
+        
+        
+        
+        
+        
         
         return True
         
