cf65e79c9915a71857870e8c1ce0dcd78812c232
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-25 19:06:45 +0900

    Time: 61 ms (19.06%), Space: 13.8 MB (97.30%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -9,32 +9,32 @@ class Solution:
         연결리스트 
         매 2개의 인접 노드끼리 스왑 
         '''
-        # 1. 값만 교환
-        # cur -> head는 왜?
-        # head 그대로 두면 안되나?
-        cur = head # 인스턴스 생성이구나
-        while cur and cur.next:
-            #값만 교환
-            cur.val, cur.next.val = cur.next.val, cur.val
-            cur = cur.next.next # 2칸씩 움직임
-        return head
+        # # 1. 값만 교환 - 67ms
+        # # cur -> head는 왜?
+        # # head 그대로 두면 안되나?
+        # cur = head # 인스턴스 생성이구나
+        # while cur and cur.next:
+        #     #값만 교환
+        #     cur.val, cur.next.val = cur.next.val, cur.val
+        #     cur = cur.next.next # 2칸씩 움직임
+        # return head
         
-#         # 2. 반복 구조
-#         root = prev = ListNode(None)
-#         prev.next = head
-#         while head and head.next:
-#             # b가 a(head)를 가리키도록 할당
-#             b = head.next
-#             head.next = b.next
-#             b.next = head
+        # 2. 반복 구조
+        root = prev = ListNode(None)
+        prev.next = head
+        while head and head.next:
+            # b가 a(head)를 가리키도록 할당
+            b = head.next
+            head.next = b.next
+            b.next = head
             
-#             # prev가 b를 가리키도록 할당
-#             prev.next = b
+            # prev가 b를 가리키도록 할당
+            prev.next = b
             
-#             # 다음번 비교를 위해 이동
-#             head = head.next
-#             prev = prev.next.next
-#         return root.next
+            # 다음번 비교를 위해 이동
+            head = head.next
+            prev = prev.next.next
+        return root.next
     
 #         # 3. 재귀 구조
 #         if head and head.next:
