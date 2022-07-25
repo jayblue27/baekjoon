277c6107e8784220df916a855b62d9e106ddc0ed
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-25 17:31:34 +0900

    Time: 48 ms (72.49%), Space: 14 MB (32.33%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -32,27 +32,36 @@ class Solution:
         # 연결리스트 문제가 실제 코테에 나오는지?
         # 다른 해결 방법은 없나?
         
-        # 인터넷 검색
+        # 인터넷 검색 풀이법 - 63ms
         # 빈노드 생성
-        res = ListNode()
-        node = res
-
+        result = ListNode()
+        node = result
+        
+        # 둘다 비어있지 않으면
         while list1 and list2:
+            # list1 값이 작으면
             if list1.val < list2.val:
-                node.next = list1
-                list1 = list1.next
-            else:
-                node.next = list2
-                list2 = list2.next
+                node.next = list1 # list1 넣고
+                list1 = list1.next # list1을 next로 갱신
+            # list2 값이 작으면
+            else: 
+                node.next = list2 # list2 넣고
+                list2 = list2.next # list2를 next로 갱신
             node = node.next
-
+        
+        # 둘 중에 하나가 비면 while종료
+        # list1이 남아있으면 node.next에 남아있는 list1 지정
         if list1:
             node.next = list1
-
+        # list2가 남아있으면 node.next에 남아있는 list2 지정
         elif list2:
             node.next = list2
 
-        return res.next        
+        return result.next
+        # result만 반환시 0이 맨앞에 포함 됨. why?
+        # 해당 풀이가 제대로 된건가?
+        
+        
         
             
             
