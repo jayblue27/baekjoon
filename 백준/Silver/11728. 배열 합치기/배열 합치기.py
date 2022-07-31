0ec00e664a1ed88a1cc7168f57fbaf9cace64b22
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-07-31 21:36:19 +0900

    [Silver V] Title: 배열 합치기, Time: 1540 ms, Memory: 184092 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -12,28 +12,6 @@ A = list(map(int, input().split()))
 # B의 내용
 B = list(map(int, input().split()))
 
-# 풀이
-i,j = 0, 0
-
-result = []
-while len(result)<(N+M) :
-    # 예외 처리 
-    # 한쪽이 끝에 도달 했을 때, 반대 배열의 값 append 및 인덱스 증가
-    if i == N:
-        result.append(B[j])
-        j+=1
-    elif j == M:
-        result.append(A[i])
-        i+=1
-    # 그 외
-    else:
-        # 비교해서 작은 쪽 먼저 append 후 index 1씩 증가
-        if A[i] < B[j]:
-            result.append(A[i])
-            i+=1
-        else:
-            result.append(B[j])
-            j+=1
-
-#출력
-print(*result)
+A.extend(B)
+A.sort()
+print(*A)
