5931ad6c198d365a45f2fd3750b3f890b43ccdbe
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-02 21:03:21 +0900

    Time: 33 ms (100.00%), Space: 14 MB (93.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,14 +5,16 @@ class Solution:
         '''
         used = {}
         #포인터 선언
-        max_length = start = 0
-        for index, char in enumerate(s):
+        max_length = start = 0   # 0, 0 
+        for index, char in enumerate(s):  # 0, a
+            # print(index, char, max_length, start, used)
+            
             # 이미 등장했던 문자라면 'start' 위치 갱신
-            if char in used and start <= used[char]:
+            if char in used and start <= used[char]:  
                 start = used[char] + 1
             # 최대 부분 문자열 길이 갱신
             else:
-                max_length = max(max_length, index - start + 1)
+                max_length = max(max_length, index - start + 1)  
             
             # 현재 문자의 위치 삽입
             used[char] = index
