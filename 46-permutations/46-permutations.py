e561b96d68193fca9e2f14a0e77a5361678bfe76
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-03 17:11:45 +0900

    Time: 77 ms (21.19%), Space: 14.2 MB (22.42%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,26 +1,26 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         #풀이 1 : DFS - 48ms
-#         results = []
-#         prev_elements = []
+        results = []
+        prev_elements = []
         
-#         def dfs(elements):
-#             # 리프 노드일 때 결과 추가
-#             if len(elements) == 0: # elements 길이가 0인게 왜 리프노드인가 나중에 확인
-#                 results.append(prev_elements[:])
+        def dfs(elements):
+            # 리프 노드일 때 결과 추가
+            if len(elements) == 0: # elements 길이가 0인게 왜 리프노드인가 나중에 확인
+                results.append(prev_elements[:])
             
-#             # 순열 생성 재귀 호출
-#             for e in elements:
-#                 next_elements = elements[:]  # [1,2,3] / 
-#                 next_elements.remove(e) # [2,3]
+            # 순열 생성 재귀 호출
+            for e in elements:
+                next_elements = elements[:]  # [1,2,3] / 
+                next_elements.remove(e) # [2,3]
                 
-#                 prev_elements.append(e) # [1]
-#                 dfs(next_elements) # dfs([2,3])
-#                 prev_elements.pop() # 
+                prev_elements.append(e) # [1]
+                dfs(next_elements) # dfs([2,3])
+                prev_elements.pop() # 
         
-#         dfs(nums)
-#         return results
+        dfs(nums)
+        return results
     
-        #풀이 2 : itertools 
+        #풀이 2 : itertools - 45ms
         return list(itertools.permutations(nums))
         
