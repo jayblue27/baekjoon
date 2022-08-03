c27bff7d89813d0a47b500ccc6d6accbc36702af
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-03 17:10:27 +0900

    Time: 48 ms (81.91%), Space: 14.2 MB (22.42%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        #풀이 1 : DFS
+        results = []
+        prev_elements = []
+        
+        def dfs(elements):
+            # 리프 노드일 때 결과 추가
+            if len(elements) == 0: # elements 길이가 0인게 왜 리프노드인가 나중에 확인
+                results.append(prev_elements[:])
+            
+            # 순열 생성 재귀 호출
+            for e in elements:
+                next_elements = elements[:]  # [1,2,3] / 
+                next_elements.remove(e) # [2,3]
+                
+                prev_elements.append(e) # [1]
+                dfs(next_elements) # dfs([2,3])
+                prev_elements.pop() # 
+        
+        dfs(nums)
+        return results
+        
