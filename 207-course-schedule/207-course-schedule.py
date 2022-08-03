09c28bd7942acdad1794ca4076fdda10eebfc8a3
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-03 17:59:27 +0900

    Time: 169 ms (42.99%), Space: 17.6 MB (10.03%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,69 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+#         # 풀이1 : DFS - 시간초과
+#         graph = collections.defaultdict(list)
+#         #그래프 구성
+#         for x,y in prerequisites:
+#             graph[x].append(y)
+            
+#         traced = set()
+        
+#         def dfs(i):
+#             # 순환 구조이면 False
+#             if i in traced:
+#                 return False
+            
+#             traced.add(i)
+#             for y in graph[i]:
+#                 if not dfs(y):
+#                     return False
+#             # 탐색 종료 후 순환 노드 삭제
+#             traced.remove(i)
+            
+#             return True
+        
+#         # 순환 구조 판별
+#         for x in list(graph):
+#             if not dfs(x):
+#                 return False
+        
+#         return True
+    
+        # 풀이2 : 가지치기 (pruning)
+        graph = collections.defaultdict(list)
+        # 그래프 구성
+        for x,y in prerequisites:
+            graph[x].append(y)
+        
+        traced = set()
+        visited = set()
+        
+        def dfs(i):
+            # 순환 구조이면 False
+            if i in traced:
+                return False
+            # 이미 방문했으면 True
+            if i in visited:
+                return True
+            
+            traced.add(i)
+            for y in graph[i]:
+                if not dfs(y):
+                    return False
+            
+            # 탐색 종료 후 순환 노드 삭제
+            traced.remove(i)
+            # 탐색 종료 후 방문 노드 추가
+            visited.add(i)
+            
+            return True
+        # 순환 구조 판별
+        for x in list(graph):
+            if not dfs(x):
+                return False
+        
+        return True
+        
+            
+            
+        
