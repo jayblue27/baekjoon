d739a16f3ae5b668394293ad4b2616bac171bca3
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-03 17:24:30 +0900

    Time: 127 ms (89.43%), Space: 15.8 MB (86.08%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -1,23 +1,23 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # 풀이1 : DFS k개 조합
-        results = []
+#         # 풀이1 : DFS k개 조합 - 683ms
+#         results = []
         
-        def dfs(elements, start:int, k:int):
-            if k == 0:
-                results.append(elements[:])
-                return
+#         def dfs(elements, start:int, k:int):
+#             if k == 0:
+#                 results.append(elements[:])
+#                 return
             
-            # 자신 이전의 모든 값을 고정하여 재귀 호출
-            for i in range(start, n+1):
-                elements.append(i)
-                dfs(elements, i+1, k-1)
-                elements.pop()
+#             # 자신 이전의 모든 값을 고정하여 재귀 호출
+#             for i in range(start, n+1):
+#                 elements.append(i)
+#                 dfs(elements, i+1, k-1)
+#                 elements.pop()
             
-        dfs([],1,k)
-        return results
+#         dfs([],1,k)
+#         return results
         
-        # # 풀이2 : itertools 사용
-        # return list(itertools.combinations(range(1,n+1),k))
+        # 풀이2 : itertools 사용
+        return list(itertools.combinations(range(1,n+1),k))
             
         
