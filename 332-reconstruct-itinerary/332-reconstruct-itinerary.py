07f746bd7236fd3ded71b331b5ca9f341f39ab90
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-03 17:45:44 +0900

    Time: 103 ms (76.93%), Space: 14.4 MB (71.15%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.py
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -1,16 +1,33 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        # 풀이1 : DFS
+#         # 풀이1 : DFS - 124ms
+#         graph = collections.defaultdict(list)
+#         # 그래프 순서대로 구성
+#         for a,b in sorted(tickets):
+#             graph[a].append(b)
+        
+#         route = []
+#         def dfs(a):
+#             # 첫 번째 값을 읽어 어휘 순 방문
+#             while graph[a]:
+#                 dfs(graph[a].pop(0))
+#             route.append(a)
+        
+#         dfs('JFK')
+#         # 다시 뒤집어 어휘 순 결과로
+#         return route[::-1]
+    
+        # 풀이2 : 스택 연산으로 큐 연산 최적화 시도
         graph = collections.defaultdict(list)
-        # 그래프 순서대로 구성
-        for a,b in sorted(tickets):
+        # 그래프를 뒤집어서 구성
+        for a, b in sorted(tickets, reverse=True):
             graph[a].append(b)
         
         route = []
         def dfs(a):
-            # 첫 번째 값을 읽어 어휘 순 방문
+            # 마지막 값을 읽어 어휘 순 방문
             while graph[a]:
-                dfs(graph[a].pop(0))
+                dfs(graph[a].pop())
             route.append(a)
         
         dfs('JFK')
