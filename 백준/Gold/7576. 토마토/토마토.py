99e3c5a42bef4b19de975b4fd34f5c82baf14a88
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-04 13:53:44 +0900

    [Gold V] Title: 토마토, Time: 2124 ms, Memory: 106148 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,39 +1,64 @@
+#참고 풀이 - 2156ms
+'''
+M x N(가로x세로) 사이즈의 토마토 상자가 있음
+0 : 안익은 토마토
+1 : 익은 토마토
+-1 : 토마토 없음
+1(익은토마토)를 기준으로 주변(상하좌우)의 0(안익은 토마토)를 
+하루에 하나씩 익게 한다.
+모든 박스의 토마토를 익히기 위해서 소요되는 일자
+모든 박스를 익히지 못하면 -1 return
+'''
+# 2156ms 
 # bfs는 queue를 사용한다. 
 from collections import deque
 queue = deque([])
 
 #입력
 M, N = map(int, input().split())
-tomatoes = [list(map(int, input().split())) for _ in range(N)]
+tomato_box = [list(map(int, input().split())) for _ in range(N)]
 
 #방향 리스트
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1] #상, 하, 좌, 우 
 res = 0
 
+# 토마토 박스 전체 순회하면서
+# 익은 토마토 찾아서, queue에 좌표 입력
 for i in range(N):
     for j in range(M):
-        if tomatoes[i][j] == 1:
+        if tomato_box[i][j] == 1:
             queue.append([i,j])
 
-# bfs함수 
+# bfs함수 - O(n^2) ??
 def bfs():
+    # 큐 안에 좌표가 남아있으면 하나씩
+    # x,y가 헷갈릴 수 있겠다.
     while queue:
-        x, y = queue.popleft() # 토마토 좌표
-        for i in range(4):
-            nx, ny = dx[i] + x, dy[i] + y
-            if 0 <= nx < N and 0 <= ny < M and tomatoes[nx][ny] == 0:
-                # 익히고 1을 더해준다.
-                tomatoes[nx][ny] = tomatoes[x][y] + 1
+        x, y = queue.popleft() # 익은토마토 좌표 x,y에 입력
+        for i in range(4): #상화좌우 순회
+            nx, ny = x + dx[i], y + dy[i] # new_x, new_y 
+            #범위 내에 안익은 토마토(0)이 있으면
+            if (0 <= nx < N) and (0 <= ny < M) and (tomato_box[nx][ny] == 0): 
+                # 토마토를 익히고 1씩 더해간다.
+                tomato_box[nx][ny] = tomato_box[x][y] + 1
+                # 큐에 갱신된 좌표를 입력한다.
                 queue.append([nx,ny])
-            
-bfs()
-for i in tomatoes:
-    for j in i:
-        # 토마토 다 못익히면 -1
-        if j == 0:
-            print(-1)
-            exit(0)
-    # 다 익히면 최댓값
-    res = max(res, max(i))
 
-print(res-1) # 처음 시작한 부분 제외
+# 토마토 익히기
+bfs()
+
+flag = 1
+# 토마토박스 전체 돌면서 최대값 
+for i in tomato_box:
+    res = max(res, max(i))
+    # 안익은 토마토(0) 이 남아있으면 -1 출력
+    if 0 in i:
+        flag = 0
+        break
+
+if flag == 1:
+    # 1부터 시작하니까 소요일자 계산을 위해 1을 빼준다. 
+    print(res-1)
+else:
+    print(-1)
+   
