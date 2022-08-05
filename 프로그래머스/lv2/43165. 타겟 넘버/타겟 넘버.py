4116232987bac640e2ffe22d05ee4bda1727341d
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-05 17:25:21 +0900

    [level 2] Title: 타겟 넘버, Time: 2.32 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,38 @@
+from collections import deque
+# # BFS with deque
+# def solution(numbers, target):
+#     answer = 0 
+#     queue = deque()  # queue 생성
+#     n = len(numbers) # numbers 길이
+    
+#     queue.append([ numbers[0],0]) # +number, 0번 index
+#     queue.append([-numbers[0],0]) # -number, 0번 index
+    
+#     while queue:         
+#         temp, idx = queue.popleft()
+#         idx += 1 
+#         # 모든 number 순환 전까지는 모든 경우의 수 만들어서 append
+#         if idx < n: 
+#             queue.append([temp+numbers[idx], idx])
+#             queue.append([temp-numbers[idx], idx])
+#         # temp 값이 target 값이면 count 1개씩 올리기
+#         else:
+#             if temp == target:
+#                 answer += 1
+#     return answer
+
+# 재귀 함수
+def solution(numbers, target):
+    n = len(numbers) #길이
+    answer = 0
+    def dfs(idx, result):  # dfs 함수
+        if idx == n:       # idx가 끝에 도달하고
+            if result == target:  # 값이 target에 도착하면
+                nonlocal answer  
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
