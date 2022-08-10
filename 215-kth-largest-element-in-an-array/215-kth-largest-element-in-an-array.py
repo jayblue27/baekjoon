0621e333cbbe24823b2d654cd6bda84a18c42e7c
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-08-10 21:14:15 +0900

    Time: 991 ms (16.99%), Space: 26.4 MB (20.86%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -3,13 +3,22 @@ class Solution:
         '''
         정렬되지 않은 배열에서 k번째 큰 요소를 추출하라. 
         '''
-        # 풀이 1 - heapq모듈 이용
-        heap = list()
-        for n in nums:
-            heapq.heappush(heap, -n)
+#         # 풀이 1 - heapq모듈 이용 - 882ms
+#         heap = list()
+#         for n in nums:
+#             heapq.heappush(heap, -n)
         
-        for _ in range(1, k):
-            heapq.heappop(heap)
+#         for _ in range(1, k):
+#             heapq.heappop(heap)
             
-        return -heapq.heappop(heap)
+#         return -heapq.heappop(heap)
+    
+        # 풀이 2 - heapq 모듈의 heapify 이용
+        heapq.heapify(nums)
+        
+        for _ in range(len(nums) - k):
+            heapq.heappop(nums)
+            
+        return heapq.heappop(nums)
+    
         
