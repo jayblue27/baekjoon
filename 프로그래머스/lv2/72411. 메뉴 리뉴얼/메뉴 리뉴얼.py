2ca333aadc9a6748971c86ca43d1fafda3910bbf
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-09-08 17:13:32 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,28 +1,30 @@
 '''
-레스토랑 
-코로나 불경기 극복 위한 메뉴 리뉴얼
+규칙1 : 최소 2가지 이상의 단품메뉴
+규칙2 : 최소 2명 이상의 손님으로부터 주문된 단품 메뉴
+규칙3 : 가장 많이 주문된 조합
 
-단품 only -> 조합을 통한 코스요리
-가장 많이 함께 주문한 단품 메뉴들 
-
-최소 2가지 이상 
-최소 2명 이상 손님 주문 
+오름차순 정렬
+메뉴 구성이 여러 개라면 모두 배열에 담는다.
 '''
 
 from itertools import combinations
 from collections import Counter
 
+# orders : 단품 메뉴 종류
+# course : 메뉴 구성 수
 def solution(orders, course):
     answer = []
-
+    
     for num in course:
-        
         temp = []
+        
         for order in orders:
-            combi = combinations(sorted(order), num)
+            combi = combinations(sorted(order), num)            
             temp += combi
-            
+                
         counter = Counter(temp)
+        print(counter)
+        
         if len(counter) != 0 and max(counter.values()) != 1:
             answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
 
