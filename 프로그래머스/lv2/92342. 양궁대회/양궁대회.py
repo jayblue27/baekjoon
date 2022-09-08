df99a556c570b0b298c52b27d2257ca4234b2a4c
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-09-08 18:32:42 +0900

    [level 2] Title: 양궁대회, Time: 2.73 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.py
@@ -1,29 +1,53 @@
-from itertools import combinations_with_replacement
+from collections import deque
+
+def bfs(n, info):    
+    res = []
+    q = deque([(0, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])])
+    maxGap = 0
+    
+    while q:
+        focus, arrow = q.popleft()
+        
+        if sum(arrow) == n:  # 종료조건 1) 화살 다 쏜 경우
+            apeach, lion = 0, 0
+            for i in range(11):
+                if not (info[i] == 0 and arrow[i] == 0):
+                    if info[i] >= arrow[i]:
+                        apeach += 10 - i
+                    else:
+                        lion += 10 - i
+            if apeach < lion:  # 라이언이 이기면
+                gap = lion - apeach
+                if maxGap > gap:
+                    continue
+                if maxGap < gap:
+                    maxGap = gap  # 최대점수차 갱신
+                    res.clear()
+                res.append(arrow)  # 최대점수차를 내는 화살상황 저장
+        
+        elif sum(arrow) > n:  # 종료조건 2) 화살 더 쏜 경우
+            continue
+        
+        elif focus == 10:  # 종료조건 3) 화살 덜 쏜 경우
+            tmp = arrow.copy()
+            tmp[focus] = n - sum(tmp)
+            q.append((-1, tmp))
+        
+        else:  # 화살 쏘기
+            tmp = arrow.copy()
+            tmp[focus] = info[focus]+1 
+            q.append((focus+1, tmp))  # 어피치보다 1발 많이 쏘기
+            tmp2 = arrow.copy()
+            tmp2[focus] = 0
+            q.append((focus+1, tmp2))  # 0발 쏘기
+    return res
 
 def solution(n, info):
-    answer = [-1]
-    maxGap = -1e9
-    candidates = list(combinations_with_replacement(range(0, 11), n))
-
-    for candidate in candidates:
-        info2 = [0] * 11
-        apeach, lion = 0, 0
-
-        for score in candidate:
-            info2[10 - score] += 1
-
-        for score, (a, l) in enumerate(zip(info, info2)):
-            if a == l == 0:
-                continue
-            elif a >= l:
-                apeach += (10 - score)
-            else:
-                lion += (10 - score)
-
-        if lion > apeach:
-            gap = lion - apeach
-            if gap > maxGap:
-                maxGap = gap
-                answer = info2
-
-    return answer
+    winList = bfs(n, info)
+    
+    if not winList:
+        return [-1]
+    elif len(winList) == 1:
+        return winList[0]
+    else:
+        return winList[-1]
