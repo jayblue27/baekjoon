4b31d22da66d9b3f157fa8b2ae25f3536e40913a
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-09-26 22:26:08 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 264 ms, Memory: 31860 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -0,0 +1,10 @@
+from sys import stdin
+
+for _ in range(int(stdin.readline())):
+    n = int(stdin.readline())
+    arr = list(map(int, stdin.readline().split()))
+    arr.sort()
+    res = 0
+    for i in range(2, n):
+        res = max(res, abs(arr[i] - arr[i-2]))
+    print(res)
