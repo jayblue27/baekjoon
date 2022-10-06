a60972be912188c71a5d8b5155e1611f96a76942
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-10-06 17:50:42 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,8 +1,40 @@
-from collections import deque
+# 얼음 얼려먹기 처럼 풀면 되겠다. 
+# 서로 연결되어 있는 요소가 몇 개인지 구하는 문제
 
-def solution(n, computers):
+# DFS- 재귀횟수 초과 df를 너무 많이 썼나? -> 재귀 제한 100만으로 늘려도 안된다. 
+# import sys
+# sys.setrecursionlimit(1000000)
+
+# def solution(n, computers):
     
-    computers = deque(computers)
+#     def dfs(j,i):
+#         if j <= -1 or j>= n or i <= -1 or i >=n:
+#             return False
+    
+#         nonlocal computers
+#         if computers[j][i] == 1: # 해당위치에 컴퓨터가 있는경우
+#             computers[j][i] == 0 # 방문처리
+#             # 재귀호출 
+#             dfs(j-1, i)
+#             dfs(j+1, i)
+#             dfs(j, i-1)
+#             dfs(j, i+1)
+#             return True
+#         return False
+
+#     n_network = 0
+#     for j in range(n):
+#         for i in range(n):
+#             if dfs(j,i) == True:
+#                 n_network += 1
+                
+#     return n_network
+
+
+
+from collections import deque
+def solution(n, computers):
+    # computers = deque(computers)
     network = 0
     global visited
     visited = [False] * n
