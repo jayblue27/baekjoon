ea0604a4e9c240b99e98b653adc24cdc777361c4
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-10-06 18:34:10 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -3,25 +3,32 @@ from collections import deque
 def solution(begin, target, words):
     answer = 0
     q = deque()
-    q.append([begin, 0])
-    V = [ 0 for i in range(len(words))]
+    q.append((begin, 0))
+    visited = [0] * len(words)
+    
+    # 예외처리
+    # target이 words에 없으면 변환하지 못한다. 
+    if target not in words:
+        return 0
     
     # 한글자만 다를때
     while q:
         word, cnt = q.popleft()
         # 타겟 단어이면 answer 갱신 및 종료
+
+        
         if word == target:
-            answer = cnt
+            # answer = cnt
             break        
         
         for i in range(len(words)):
             temp_cnt = 0
-            if not V[i]:
+            if not visited[i]:
                 for j in range(len(word)):
                     if word[j] != words[i][j]:
                         temp_cnt += 1
                 if temp_cnt == 1:
                     q.append([words[i], cnt+1])
-                    V[i] = 1
+                    visited[i] = 1
                     
-    return answer
+    return cnt
