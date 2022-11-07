928d16e7b523aab261307d773d8c3f7b0f6667c0
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-11-07 09:52:13 +0900

    [Silver I] Title: 경로 찾기, Time: 264 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,13 @@
+n=int(input())
+G = []
+for _ in range(n):
+    G.append(list(map(int,input().split())))
+    
+for k in range(n): 
+    for i in range(n):
+        for j in range(n): 
+            if G[i][j] == 1 or (G[i][k] == 1 and G[k][j] == 1):
+                G[i][j] = 1
+
+for row in G:
+    print(*row)
