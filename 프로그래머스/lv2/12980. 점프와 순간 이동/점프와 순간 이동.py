41661ef00ffc1ac843e30905ae4767264b2f9252
Hoyeong Jeong <jay.jeong0082@gmail.com>
2022-12-08 18:24:09 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,22 +1,7 @@
-'''
-0부터 n까지 이동
-- 점프하면 1칸에 에너지 1씩 소모
-- 순간이동(현재까지 이동한 거리의 2배) 에너지 소모 없음 
-'''
-
-# # 시간초과
-# def solution(n):
-#     ans = 0
-#     while n != 0:
-#         print(n)
-#         if n%2==0:
-#             n //= 2
-#         else: 
-#             n-=1
-#             ans+=1
-
-#     return ans
-
-# 참고
 def solution(n):
-    return bin(n).count('1')
+    ans = 0
+    
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return ans
