982e332cebd4ebee2afb77e89ad7e60dd9278ee6
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-01-02 04:20:12 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -4,38 +4,40 @@
 -- 작성일, 리뷰텍스트 오름차순 정렬
 
 -- 리뷰를 가장 많이 작성한 회원의 id
-# SELECT *
+# SELECT
 #     MEMBER_NAME,
 #     REVIEW_TEXT,
 #     DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d')
 # FROM REST_REVIEW
 # LEFT JOIN MEMBER_PROFILE
 # USING(MEMBER_ID)
-# WHERE MEMBER_ID IN (
-#     SELECT MEMBER_ID
-#     FROM REST_REVIEW
-#     GROUP BY MEMBER_ID
-#     HAVING COUNT(*) = (
-#         SELECT MAX(cnt) AS MAX_CNT
-#         FROM (
-#             SELECT COUNT(*) AS CNT 
-#             FROM REST_REVIEW 
-#             GROUP BY MEMBER_ID) AS sub) )
+# WHERE MEMBER_ID IN (SELECT MEMBER_ID
+#                     FROM REST_REVIEW
+#                     GROUP BY MEMBER_ID
+#                     HAVING COUNT(*) = (SELECT MAX(CNT)
+#                                        FROM (SELECT COUNT(*) AS CNT 
+#                                              FROM REST_REVIEW 
+#                                              GROUP BY MEMBER_ID) AS sub) )
 # ORDER BY REVIEW_DATE, REVIEW_TEXT
 
-
-SELECT 
-    mp.MEMBER_NAME, 
-    rr.REVIEW_TEXT, 
-    substr(rr.REVIEW_DATE,1,10) as REVIEW_DATE
-from MEMBER_PROFILE mp
-left join REST_REVIEW rr
-on mp.MEMBER_ID = rr.MEMBER_ID 
-where rr.MEMBER_ID = (
-    select MEMBER_ID
-    from REST_REVIEW
-    group by MEMBER_ID
-    order by count(*) desc
-    limit 1
-)
-order by rr.REVIEW_DATE, rr.REVIEW_TEXT
+SELECT
+    mp.MEMBER_NAME,
+    rr.REVIEW_TEXT,
+    DATE_FORMAT(rr.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+FROM REST_REVIEW AS rr
+LEFT JOIN MEMBER_PROFILE AS mp
+USING(MEMBER_ID)
+-- MEMBER ID가 IN 내부에 해당하는 경우 FILTER
+WHERE MEMBER_ID IN (
+    -- COUNT값이 HAVING절에 해당하는 경우
+    SELECT MEMBER_ID
+    FROM REST_REVIEW
+    GROUP BY MEMBER_ID
+    HAVING COUNT(*) = (
+        -- HAVING : MEMBER ID별 가장 많은 리뷰 수
+        SELECT COUNT(*) AS CNT 
+        FROM REST_REVIEW 
+        GROUP BY MEMBER_ID
+        ORDER BY CNT DESC
+        LIMIT 1))
+ORDER BY REVIEW_DATE, REVIEW_TEXT
