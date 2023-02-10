f936d53a8dd9621f1629b1c0b3eea229d018838d
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-02-10 19:34:58 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -6,13 +6,9 @@ def solution(t, p):
     # 부분 숫자 다 만들고
     for i in range(len(t)-n+1):
         arr.append(int(t[i:i+n]))
-        
+    
+    # 정렬 후 
     arr.sort()
+    # 2분탐색 통해서 index 반환
     return bisect_right(arr, int(p))
     
-    # return arr
-        
-    # 오름차순 정렬 
-    
-    # answer = 0
-    # return answer
