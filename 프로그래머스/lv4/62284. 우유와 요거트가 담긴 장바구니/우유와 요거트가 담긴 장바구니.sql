a5dbde00607fd1b8cdea22702be5b8cfcbabfe40
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-03-03 20:48:18 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/lv4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -8,20 +8,25 @@ Milk와 Yogurt르르 동시에 구입한 장바구니(CART_ID)
 - 정렬
 CART_ID 
 
+GROUP_CONCAT(필드) + GROUP BY 활용 WHERE AND 적용
 */
 
-# -- 코드를 입력하세요
+# 코드를 입력하세요
+
+-- milk 구매한 테이블 
 WITH milk AS (
     SELECT *
     FROM CART_PRODUCTS
     WHERE NAME = 'milk'
 ),
 
+-- yogurt 구매한 테이블
 yogurt AS (
     SELECT *
     FROM CART_PRODUCTS
     WHERE NAME = 'yogurt')
 
+-- 쿼리
 SELECT CART_ID
 FROM milk
 INNER JOIN yogurt
