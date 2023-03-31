607980943b5dde7badd9404eae6f5b253e8dd67a
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-03-31 19:59:20 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,18 +1,13 @@
-# '''
-# progresses가 100% 일 때 서비스에 반영가능
-# 빠른 기능이 먼저 완료되어야 한다.
+'''
+progresses가 100% 일 때 서비스에 반영가능
+빠른 기능이 먼저 완료되어야 한다.
 
-# 1. 프로그레스를 100에서 뺀 값으로 바꿔주고
-# 2. 그 값을 speeds 몫 연산자로 
-# '''
-# def solution(progresses, speeds):
-#     answer = []    
-#     return answer
-
-
-
-
-
+1. 프로그레스를 100에서 뺀 값으로 바꿔주고
+2. 그 값을 speeds 몫 연산자로 
+'''
+def solution(progresses, speeds):
+    answer = []    
+    return answer
 
 # 프로그래머스 - 기능개발 
 def solution(progresses, speeds):
