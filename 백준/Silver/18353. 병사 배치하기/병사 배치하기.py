d9cc98dc7d88b2fb138eef52a687f19c189db35d
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-06-01 23:14:13 +0900

    [Silver II] Title: 병사 배치하기, Time: 792 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -0,0 +1,13 @@
+import sys
+f = sys.stdin.readline
+
+n = int(f())
+a = [int(x) for x in f().split()]
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if a[i] < a[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(len(a) - max(dp))
