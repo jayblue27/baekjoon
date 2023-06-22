13b5d7b4cadb51b4d6d5e34112141552a1df91b1
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-06-22 23:16:10 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 500 ms, Memory: 74744 KB -BaekjoonHub

---
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+fibo = [0 for _ in range(1500000)]
+n = int(stdin.readline())
+fibo[0:1] = [0, 1]
+
+if n < 0:
+    for i in range(-1, n - 1, -1):
+        data = fibo[i+2] - fibo[i+1]
+        if data < 0:
+            fibo[i] = (abs(data) % 1000000000) * -1
+        else:
+            fibo[i] = data % 1000000000
+    if fibo[n] < 0:
+        print(-1)
+        print(fibo[n] * -1)
+    else:
+        print(1)
+        print(fibo[n])
+
+
+elif n > 0:
+    for i in range(2, n + 1):
+        fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1000000000
+
+    print(1)
+    print(fibo[n])
+
+else:
+    print(0)
+    print(0)
