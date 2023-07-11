c1e1fa77c27bd8c39e6b02911cf3ad2c34b60bcc
Hoyeong Jeong <jay.jeong0082@gmail.com>
2023-07-11 16:09:18 +0900

    [Silver III] Title: N과 M (3), Time: 1440 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,13 +1,21 @@
-N,M = map(int, input().split())
+def solution(N,M):
+    res = []
+    v = [False] * (N+1)
 
-def dfs(N,M,arr=[]): #dfs
-    if len(arr) == M: 
-        print(' '.join(map(str, arr))) 
-        return 
-    
-    for i in range(1,N+1): 
-        arr.append(i) 
-        dfs(N,M) 
-        arr.pop() 
-        
-dfs(N,M)
+    def backtracking(num):
+        # breakpoint()
+        # 종료 조건
+        if num == M:
+            print(' '.join(map(str, res)))
+            return
+        for i in range(1, N+1):
+            if not v[i]:
+                # v[i] = True
+                res.append(i)
+                backtracking(num+1) #하나씩 늘려감 
+                # v[i] = False
+                res.pop()
+    backtracking(0)
+N,M = map(int,input().split())
+# N, M = 4, 2
+solution(N,M)
