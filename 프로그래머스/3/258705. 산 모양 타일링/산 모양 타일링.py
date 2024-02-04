5b7274899350319a278e4fbcb7d1363ceb64bcc4
Hoyeong Jeong <jay.jeong0082@gmail.com>
2024-02-04 20:43:19 +0900

    [level 3] Title: 산 모양 타일링, Time: 43.39 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -0,0 +1,21 @@
+'''
+
+'''
+
+def solution(n, tops):
+    MOD = 10007
+    a =[0] * (n+1)
+    b = [0] * (n+1)
+    
+    a[0] = 0
+    b[0] = 1
+    
+    for k in range(1,n+1):
+        # 위에 뾰족이가 있다면 
+        if tops[k-1]:
+            a[k] = (a[k-1]+ b[k-1]) % MOD
+            b[k] = (2*a[k-1] + 3*b[k-1]) %MOD
+        else:
+            a[k] = (a[k-1]+ b[k-1]) % MOD
+            b[k] = (a[k-1] +2 * b[k-1]) % MOD
+    return (a[n] + b[n]) % MOD
